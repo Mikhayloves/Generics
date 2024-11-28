ad28965e2d34cfd124f8c94e2900fcc8ddd2a9ed
Mikhaylovskiy Anton <toni_mikhaylovskiy@mail.ru>
2024-11-28 19:21:02 +0300

    Rename package

--- src/main/java/org/sberuniversity/CountMapImpl/CountMapImpl.java
+++ src/main/java/org/sberuniversity/CountMapImpl/CountMapImpl.java
@@ -6,31 +6,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CountMapImpl<T> implements CountMap<T> {
-    private final Map<T, Integer> map = new HashMap<>();
+    Map<T, Integer> map = new HashMap<>();
 
-
+    // добавляет элемент в этот контейнер.
     @Override
     public void add(T t) {
         map.put(t, map.getOrDefault(t, 0) + 1);
     }
-
+    //Возвращает количество добавлений данного элемента
     @Override
     public int getCount(T t) {
         return map.getOrDefault(t, 0);
     }
-
+    //Удаляет элемент и контейнера и возвращает количество его добавлений(до удаления)
     @Override
     public int remove(T t) {
         map.getOrDefault(t, 0);
         return map.remove(t);
     }
 
-
+    //количество разных элементов
     @Override
     public int size() {
         return map.size();
     }
 
+    //Добавить все элементы из source в текущий контейнер,
+    // при совпадении ключей,     суммировать значения
     @Override
     public void addAll(CountMap<T> source) {
         Map<T, Integer> sourceMap = source.toMap();
@@ -40,7 +42,8 @@ public class CountMapImpl<T> implements CountMap<T> {
             map.put(key, map.getOrDefault(key, 0) + value);
         }
     }
-
+    //Вернуть java.util.Map. ключ - добавленный элемент,
+    // значение - количество его добавлений
     @Override
     public Map<T, Integer> toMap() {
         return new HashMap<>(map);
--- src/main/java/org/sberuniversity/Main.java
+++ src/main/java/org/sberuniversity/Main.java
@@ -3,6 +3,9 @@ package org.sberuniversity;
 import org.sberuniversity.CountMap.CountMap;
 import org.sberuniversity.CountMapImpl.CountMapImpl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         CountMap<Integer> map = new CountMapImpl<>();
@@ -12,11 +15,23 @@ public class Main {
         map.add(5);
         map.add(5);
         map.add(5);
+        map.add(4);
+
+
 
         System.out.println("Количество добавлений элемента 5: " + map.getCount(5));
         System.out.println("Количество добавлений элемента 6: " + map.getCount(6));
         System.out.println("Количество добавлений элемента 10: " + map.getCount(10));
 
         System.out.println("Количество разных элементов: " + map.size());
+        System.out.println("Количство повторений элемента до удаления: " + map.remove(5));
+        System.out.println("Количество разных элементов " + map.size());
+
+        map.addAll(map);
+        Map<Integer, Integer> toMapCheckMap = map.toMap();
+        System.out.println(toMapCheckMap);
+        Map<Integer, Integer> toCheckMap = new HashMap<>();
+        map.toMap(toCheckMap);
+        System.out.println(toCheckMap);
     }
 }
